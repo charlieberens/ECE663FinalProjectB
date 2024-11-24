Fixed overzealous masking. Adding bitwise normalization. Added CLIP

main.py
@@ -93,11 +93,19 @@ def main():
             start_epoch=start_epoch,
             experiment_name=args.name)
         
-        watermark_mask, hash_mask = create_mask(
-            H=args.size,
-            W=args.size,
-            args=args.masking_args
-        )
+        if args.hash_mode == "alternate":
+            watermark_mask, hash_mask = create_mask(
+                H=args.size,
+                W=args.size,
+                args=args.masking_args
+            )
+        else:
+            watermark_mask = torch.ones(
+                (3,args.size, args.size)
+            )
+            hash_mask = torch.ones(
+                (3, args.size, args.size)
+            )
 
         noise_config = args.noise if args.noise is not None else []
         hidden_config = HiDDenConfiguration(H=args.size, W=args.size,
@@ -121,6 +129,8 @@ def main():
             pickle.dump(train_options, f)
             pickle.dump(noise_config, f)
             pickle.dump(hidden_config, f)
+
+        # torch.autograd.set_detect_anomaly(True)
 
 
     logging.basicConfig(level=logging.INFO,

model/decoder.py
@@ -1,8 +1,9 @@
+import math
 import torch
 import torch.nn as nn
 from options import HiDDenConfiguration
 from model.conv_bn_relu import ConvBNRelu
-
+from torchvision import transforms
 
 class Decoder(nn.Module):
     """
@@ -64,15 +65,24 @@ class BitwiseDecoder(nn.Module):
         self.num_bits = int(config.masking_args)
         self.int_rounding_factor = 2**self.num_bits
 
+        # We take the bits to be uniformly distributed on the interval [0,1]. Therefore, their mean should be .5, and their standard deviation should be sqrt((b-a)^2/12) = (sqrt(1/12))
+        root_one_twelveth = 1/math.sqrt(12)
+        self.bit_norm = transforms.Normalize(
+            [.5, .5, .5],
+            [root_one_twelveth, root_one_twelveth, root_one_twelveth]
+        )
+
     def forward(self, image_with_wm):
         if self.mask != None:
             x = self.mask * image_with_wm
         
         rounded_down_image = torch.floor(
             image_with_wm * 256 / self.int_rounding_factor) * self.int_rounding_factor / 256
-        image_with_wm = image_with_wm - rounded_down_image
+        
+        last_few_bits = image_with_wm - rounded_down_image
+        last_few_bits = self.bit_norm(last_few_bits)
 
-        x = self.layers(image_with_wm)
+        x = self.layers(last_few_bits)
         # the output is of shape b x c x 1 x 1, and we want to squeeze out the last two dummy dimensions and make
         # the tensor of shape b x c. If we just call squeeze_() it will also squeeze the batch dimension when b=1.
         x.squeeze_(3).squeeze_(2)

model/encoder.py
@@ -1,9 +1,9 @@
+import math
 import torch
 import torch.nn as nn
 from options import HiDDenConfiguration
 from model.conv_bn_relu import ConvBNRelu
-from torchvision import datasets, transforms
-
+from torchvision import transforms
 
 class Encoder(nn.Module):
     """
@@ -144,8 +144,16 @@ class BitwiseEncoder2(nn.Module):
         self.bit_change_temp = 10
         self.include_image = include_image
 
+        # We take the bits to be uniformly distributed on the interval. Therefore, their mean should be .5, and their standard deviation should be sqrt((b-a)^2/12) = (sqrt(1/12))
+        root_one_twelveth = 1/math.sqrt(12)
+
         self.bit_norm = transforms.Normalize(
-            [0.5 * 1/self.int_rounding_factor, 0.5 * 1/self.int_rounding_factor, 0.5 * 1/self.int_rounding_factor], [0.5 * 1/self.int_rounding_factor, 0.5 * 1/self.int_rounding_factor, 0.5 * 1/self.int_rounding_factor]
+            [.5, .5, .5],
+            [root_one_twelveth, root_one_twelveth, root_one_twelveth]
+        )
+        self.undo_bit_norm = transforms.Normalize(
+            [-.5, -.5, -.5],
+            [1/root_one_twelveth, 1/root_one_twelveth, 1/root_one_twelveth]
         )
         self.img_norm = transforms.Normalize(
             [0.5, 0.5, 0.5], [0.5, 0.5, 0.5]
@@ -159,9 +167,8 @@ class BitwiseEncoder2(nn.Module):
 
         if self.include_image:
             normed_image = self.img_norm(image)
-        # image = self.norm(pre_norm_image)
 
-        # last_few_bits = self.bit_norm(last_few_bits)
+        last_few_bits = self.bit_norm(last_few_bits)
 
         # First, add two dummy dimensions in the end of the message.
         # This is required for the .expand to work correctly